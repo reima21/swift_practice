876ee59c659db6dd5c460ac7469fe1623b798cd9
reima21 <matsudaira@matsudairareijinoMacBook-puro.local>
2019-04-26 17:43:48 +0900

    RxSwiftの簡単なサンプルを作成

--- RxSwift/rxswift_textfield/rxswift_textfield/ViewController.swift
+++ RxSwift/rxswift_textfield/rxswift_textfield/ViewController.swift
@@ -11,10 +11,21 @@ import RxSwift
 import RxCocoa
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var textField: UITextField!
+    
+    let disposeBag = DisposeBag()
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        textField.rx.text.orEmpty
+            .map{$0.description}
+            .bind(to: label.rx.text)
+            .disposed(by: disposeBag)
+
     }
 
 
