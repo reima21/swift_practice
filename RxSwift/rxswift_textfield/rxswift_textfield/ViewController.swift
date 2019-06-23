d830de5c287f71ab37da033d4f1474fdc7fe2a0c
reima21 <matsudaira@matsudairareijinoMacBook-puro.local>
2019-06-23 17:27:01 +0900

    進捗追加

--- RxSwift/rxswift_textfield/rxswift_textfield/ViewController.swift
+++ RxSwift/rxswift_textfield/rxswift_textfield/ViewController.swift
@@ -13,6 +13,7 @@ import RxCocoa
 class ViewController: UIViewController {
     
     @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var textLabel: UILabel!
     @IBOutlet weak var textField: UITextField!
     
     let disposeBag = DisposeBag() //観測状態からの解放を行う際に使う
@@ -25,13 +26,20 @@ class ViewController: UIViewController {
             .bind(to: label.rx.text) //受け取った値を対象のUIViewのプロパティに反映させる
             .disposed(by: disposeBag) //観測対象から除外する
         
+        textField.rx.text.orEmpty
+            .filter{$0.count >= 3}
+            .map{"入力した文字数は\($0.count)です"}
+            .bind(to: textLabel.rx.text)
+            .disposed(by: disposeBag)
+        
         
         let helloObservable : Observable<String> = Observable.from(["H","e","l","l","o"])
         helloObservable.subscribe(
-            onNext: { value in print(value) },
-            onError: { error in print(error.localizedDescription) },
+            onNext: { print($0) },
+            onError: { print($0.localizedDescription) },
             onCompleted: { print("completed") }
         ).disposed(by: disposeBag)
+        
 
     }
 
